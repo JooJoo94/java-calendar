04e8a900dd1c55b286df1eb9f40a7e7ea6d44782
USER <USER@USER-SRBCUPKCJA>
2019-07-26 17:57:08 +0900

    Leap year calendar

--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -10,18 +10,23 @@ public class Prompt {
 		Calendar cal = new Calendar();
 		Scanner sc = new Scanner(System.in);
 		
+		int month = 1;
+		int year = 0;
 		while(true) {
+			
+			System.out.println("\n년도를 입력하시오\n"); 
+			System.out.println(PROMPT);
+			year = sc.nextInt();
 			System.out.println("\n월을 입력하시오\n"); 
 			System.out.println(PROMPT); 
-			
-			int month = sc.nextInt();
+			month = sc.nextInt();
 			if(month == -1) {
 				break;
 			}
 			if(month > 12) {
 				continue;
 			}
-			cal.printSampleCalendar(2017, month);
+			cal.printSampleCalendar(year, month);
 			
 		}
 		System.out.println("Bye");
