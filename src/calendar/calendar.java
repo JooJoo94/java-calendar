e49ea3f45ab27d30524d1638c8fb7f636b591f8a
USER <USER@USER-SRBCUPKCJA>
2019-07-26 14:32:01 +0900

    Implement Maxmonth

--- src/calendar/calendar.java
+++ src/calendar/calendar.java
@@ -1,5 +1,7 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class calendar {
 
 	public static void main(String[] args) {
@@ -10,6 +12,16 @@ public class calendar {
 		System.out.println(" 8  9 10 11 12 13 14 ");
 		System.out.println("15 16 17 18 19 20 21 ");
 		System.out.println("22 23 24 25 26 27 28 ");
+		
+		//숫자를 입력받아 달의 최대 일수를 출력하는 프로그램
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month =  sc.nextInt();
+		
+		int [] Maxdays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		System.out.printf("%d월은 %d일까지 있습니다", month, Maxdays[month-1]);
+		
 		
 		
 
